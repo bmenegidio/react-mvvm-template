3e5a27065ff7aedea148e077f519e40d3cc916ac
Bruno Menegidio <brunomenegidio@hotmail.com>
2024-08-19 22:54:26 -0300

    feat: configura injeção de dependências

--- src/providers/RootProviders.tsx
+++ src/providers/RootProviders.tsx
@@ -1,17 +1,26 @@
+'use client';
+
+import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ReactNode } from 'react';
 
+import { setupDependencyInjectionContainerRegistry } from '@/DI/ioc';
 import { theme } from '@/theme/theme';
 
 type Props = {
   children: ReactNode;
 };
 
+setupDependencyInjectionContainerRegistry();
+
 export function RootProviders({ children }: Props) {
   return (
     <AppRouterCacheProvider>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
     </AppRouterCacheProvider>
   );
 }
